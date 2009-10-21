a74f0065709d26d8f478ac31a3864891fd42c9d8
Vinícius das Chagas Silva <vinimaster@blackpearl.(none)>
2009-10-21 14:42:59 -0200

    Commit inicial do projeto pythoncampus para o repositorio git

---
+++ inscricoes/tests.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+from models import Inscrito, MiniEvento, Ministrante
+
+
+class InscricaoEmMinicurso(TestCase):
+    
+    def setUp(self):
+        ministrante_grok = Ministrante.objects.create(nome='Gustavo Rezende', descricao='Mó patinho no DOTA',
+                                                           site = 'http://nsigustavo.blogspot.com')
+        
+        self.minicurso_grok = MiniEvento.objects.create(nome='Aprendendo Grok', descricao='Um curso fácil de grok',
+                                                        publico_alvo='Masoquistas', ministrante=ministrante_grok,
+                                                        data='2012-12-12', horario='16:00:00',
+                                                        local='Laboratório 01', tipo='minicurso', vagas_disponiveis=1)
+        
+        self.jim_fulton = Inscrito.objects.create(nome='Jim Fulton', instituicao='Zope Corporation',cpf='12345678910')
+    
+    def test_cadastrar_1_no_minicurso(self):
+        self.jim_fulton.inscrever(self.minicurso_grok)
+        self.assertEquals(0, self.minicurso_grok.vagas_disponiveis)
+    
+    def test_cadastrar_2_no_minicurso(self):        
+        martin_aspeli = Inscrito.objects.create(nome='Martin Aspeli', instituicao='Plone Foundation',cpf='12345678911')
+        
+        self.jim_fulton.inscrever(self.minicurso_grok)
+        try:
+            martin_aspeli.inscrever(self.minicurso_grok)
+            self.fail('Limite de vagas do minicurso não respeitado')
+        except:
+            pass
+        
+        self.assertEquals(0, len(martin_aspeli.minicurso.all()))
+       
+        
+
---
+++ settings.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Django settings for pythoncampus project.
+
+import os
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Vanderson Mota', 'vanderson.mota@gmail.com'),
+    ('Vinícius Chagas', 'vinimaster@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = os.path.join(PROJECT_ROOT_PATH,'pythoncampus.db')
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Sao_Paulo'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'pt-br'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH, 'media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'r&e+fl098tu8^n)rpyiw!90_(b2yb&_ia^r(xn&#75@oko149q'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'pythoncampus.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT_PATH,'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'inscricoes',
+)
