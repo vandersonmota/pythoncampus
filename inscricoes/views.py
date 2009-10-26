6c11b6a01abfc37fc9db697ba2c51f8b309ce65c
Vinícius das Chagas Silva <vinimaster@blackpearl.(none)>
2009-10-26 16:48:23 -0200

    Filtragem de palestra e minicurso no modo roubado :)

--- inscricoes/views.py
+++ inscricoes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic.create_update import create_object
 from django.http import HttpResponseRedirect
-from models import Palestra, Inscrito, MiniCurso
+from models import Palestra, Inscrito, MiniCurso, DadosMiniEvento
 from forms import FormularioInscrito
 
 def inscricao_base_view(request):
@@ -13,19 +13,12 @@ def inscricao_base_view(request):
     )
     
 def programacao(request):
-    palestras_dia_27 = Palestra.objects.filter(
-        tipo='palestra',
-        data__day=27).order_by('horario')
-    palestras_dia_28 = Palestra.objects.filter(
-        tipo='palestra',
-        data__day=28).order_by('horario')
+    import ipdb;ipdb.set_trace()
+    palestras_dia_27 = Palestra.objects.filter(minievento=DadosMiniEvento.objects.filter(data__day = 27).order_by('horario'))
+    palestras_dia_28 = Palestra.objects.filter(minievento=DadosMiniEvento.objects.filter(data__day = 28).order_by('horario'))
     
-    minicursos_dia_27 = MiniCurso.objects.filter(
-        tipo='minicurso',
-        data__day=27).order_by('horario')
-    minicursos_dia_28 = MiniCurso.objects.filter(
-        tipo='minicurso',
-        data__day=28).order_by('horario')
+    minicursos_dia_27 = MiniCurso.objects.filter(minievento=DadosMiniEvento.objects.filter(data__day = 27).order_by('horario'))
+    minicursos_dia_28 = MiniCurso.objects.filter(minievento=DadosMiniEvento.objects.filter(data__day = 28).order_by('horario'))
     
     return render_to_response(
         'programacao.html',
