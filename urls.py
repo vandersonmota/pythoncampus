f91919db0b23850210bf64b26ae30537fd5565eb
Vinícius das Chagas Silva <vinimaster@blackpearl.(none)>
2009-10-27 14:03:51 -0200

    Adicionado layout da programação;Adicionada listagem de minicursos

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from inscricoes.models import Palestra, Ministrante
+from inscricoes.models import Palestra, Ministrante, MiniCurso
 from inscricoes.forms import FormularioInscrito
 
 # Uncomment the next two lines to enable the admin:
@@ -9,18 +9,33 @@ admin.autodiscover()
 
 object_list = 'django.views.generic.list_detail.object_list'
 object_detail = 'django.views.generic.list_detail.object_detail'
+create_object = 'django.views.generic.create_update.create_object'
+
+propriedades_inscricao = {
+    'form_class': FormularioInscrito,
+    'template_name': 'inscricao.html',
+    'post_save_redirect': '/'
+    }
+
+propriedades_ministrante = {
+    'queryset': Ministrante.objects.all(),
+    'template_name':'detalhes_ministrante.html',
+    }
+
+propriedades_minicursos = {
+    'queryset': MiniCurso.objects.all().order_by('data').order_by('horario'),
+    'template_name': 'minicursos.html',
+    }
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^$','views.index'),
-    (r'^inscricao_principal/$','inscricoes.views.inscricao_base_view'),
-    (r'^inscricao/$','django.views.generic.create_update.create_object',
-        {'form_class': FormularioInscrito,'template_name': 'inscricao.html',
-        'post_save_redirect': '/'}),
+    (r'^inscricao_principal/$', 'inscricoes.views.inscricao_base_view'),
+    (r'^inscricao/$', create_object, propriedades_inscricao),
     (r'^programacao/', 'inscricoes.views.programacao'),
-    (r'^ministrante/(?P<object_id>\d+)',object_detail,
-     {'queryset': Ministrante.objects.all(),'template_name':'detalhes_ministrante.html',}
-    ),
+    (r'^minicursos/', object_list, propriedades_minicursos),
+    (r'^ministrante/(?P<object_id>\d+)', object_detail, propriedades_ministrante),
     (r'^media/(.*)$', 'django.views.static.serve',
-    {'document_root': settings.MEDIA_ROOT}),
+        {'document_root': settings.MEDIA_ROOT}
+    ),
 )
