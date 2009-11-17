ae78e5505c58dc98db774c9d17b0f530e5dc4a8c
Vanderson Mota <vanderson@vanderson-laptop.(none)>
2009-11-17 17:24:24 -0200

    Mensagem de aviso para confirmação de inscrição e envio de email adicionado

--- inscricoes/models.py
+++ inscricoes/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.core.mail import send_mail
 
 TIPO_MINI_EVENTO = (
     ('palestra','Palestra'),
@@ -37,6 +38,10 @@ class MiniCurso(MiniEvento):
             
         self.inscrito_set.add(participante)
         self.save()
+    
+    def __unicode__(self):
+        return self.nome + u' - Data: ' + str(self.data.day) + '/' + str(self.data.month) + \
+               u' Horário: ' + self.horario.isoformat()[:5] + ' hs'
 
 class Inscrito(models.Model):
     ESTADOS = (('pendente', 'Pendente'),
@@ -46,6 +51,7 @@ class Inscrito(models.Model):
     nome = models.CharField('Nome',max_length=100)
     instituicao = models.CharField('Instituição',max_length=100)
     cpf = models.CharField('CPF',max_length=11,unique=True)
+    email = models.EmailField('E-mail')
     minicurso = models.ManyToManyField('MiniCurso', null=True, blank=True)
     estado = models.CharField(max_length=20, choices=ESTADOS, default='pendente')
     
@@ -55,6 +61,7 @@ class Inscrito(models.Model):
     def inscrever(self, minicurso):
         minicurso.registrar_participante(self)
         self.minicurso.add(minicurso)
+        send_mail(u'Confirme sua presença!', u'Atenção! Para garantir sua vaga nos minicursos, traga 1kg de arroz, feijão ou macarrão para cada minicurso no qual você estiver inscrito até 26/11/2009. O local de confirmação e arrecadação é no NSI - Núcleo de Sistemas de Informação (Sala 133, Bloco A), localizado no IFF Campos - Campus Centro (Antigo Cefet Campos), das 14:00 às 19:00 hs', 'pythoncampus@iff.edu.br', [self.email,], fail_silently=True)
 
 
 class Ministrante(models.Model):
